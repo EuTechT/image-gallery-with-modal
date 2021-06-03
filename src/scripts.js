26c64371f6a69e47c9a5f6f9c24522ab5c728abb
André Filipe <eutech.backup@gmail.com>
2021-06-03 16:43:12 -0300

    Added functionality to click arrows and use keys to move photos

--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,16 @@ let images = window.document.querySelectorAll('.small-img');
 let modal = window.document.querySelector('.modal');
 let imgModal = window.document.querySelector('#img-modal');
 let btnClose = window.document.querySelector('#btn-close');
+let btnPrevious = window.document.querySelector('#btn-previous');
+let btnNext = window.document.querySelector('#btn-next');
 
 images.forEach(
-    (image) => {
+    (image, i, a) => {
         image.addEventListener('click', () => {
             modal.classList.toggle('modal--active');
             changeImage(image.getAttribute('src'), image.getAttribute('alt'));
+            btnPrevious.style.display = 'inline';
+            btnNext.style.display = 'inline';
         });
     }
 );
@@ -22,9 +26,59 @@ btnClose.addEventListener('click', () => {
 });
 
 function closeModal(event) {
+    //console.log(event);
     if(event.key == "Escape") {
         modal.classList.remove('modal--active');
+    }
+    if(event.key == "ArrowLeft") {
+        previousImg();
+    }
+    if(event.key == "ArrowRight"){
+        nextImg();
     }
 }
 
 window.onkeydown = closeModal;
+
+btnNext.addEventListener('click', () => {
+    nextImg();
+});
+
+function nextImg() {
+    let imgModalCurrent = imgModal.src;
+    btnPrevious.style.display = 'inline';
+
+    for(let i = 0; i < images.length; i++){
+        if(imgModalCurrent == images[i].src){
+            let pos = i + 1;
+
+            if(pos == images.length) {
+                btnNext.style.display = 'none';
+            } else {
+                changeImage(images[pos].getAttribute('src'), images[pos].getAttribute('alt'));
+            }
+        }
+    }
+}
+
+btnPrevious.addEventListener('click', () => {
+    previousImg();
+});
+
+
+function previousImg() {
+    let imgModalCurrent = imgModal.src;
+    btnNext.style.display = 'inline';
+
+    for(let i = 0; i < images.length; i++){
+        if(imgModalCurrent == images[i].src){
+            let pos = i - 1;
+
+            if(pos < 0) {
+                btnPrevious.style.display = 'none';
+            } else {
+                changeImage(images[pos].getAttribute('src'), images[pos].getAttribute('alt'));
+            }
+        }
+    }
+}
